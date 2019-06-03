aa7d7c7e13b9609db5a4a3169daa8c270103aefd
dezcorjm <dezcorjm@gmail.com>
2019-06-03 09:47:47 -0500

    reseteo de password

--- apps/estudiantes/views.py
+++ apps/estudiantes/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 
 # Create your views here.
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView
 from apps.estudiantes.models import Estudiante
 from django.contrib.auth.models import User
-from apps.estudiantes.forms import EstudianteForm, RegistroForm
+from apps.estudiantes.forms import EstudianteForm, RegistroForm, UpdateRegsForm, UpdateEstudenFrom
 from django.contrib.auth.views import LoginView
 
 class Login(LoginView):
@@ -38,3 +38,36 @@ class CrearUsuario(CreateView):
             return HttpResponseRedirect(self.get_success_url())
         else:
             return self.render_to_response(self.get_context_data(form=form,form2=form2))
+
+
+class UpdateUsuario(UpdateView):
+    model = User
+    second_model = Estudiante
+    template_name = 'estudiantes/Estudiante_form.html'
+    form_class = UpdateRegsForm
+    second_form_class = UpdateEstudenFrom
+    success_url = reverse_lazy("conferencia:index")
+
+    def get_context_data(self, **kwargs):
+        context = super(UpdateUsuario,self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk',0)
+        user = self.request.user
+        estudiante = self.second_model.objects.get(user = user)
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'form2' not in context:
+            context['form2'] = self.second_form_class(instance= estudiante)
+
+        return context
+    
+    def post(self,request,*args,**kwargs):
+        self.object = self.get_object
+        NUA = kwargs['pk']
+        user = self.request.user
+        estudiante = self.second_model.objects.get(user = user)
+        form = self.form_class(request.POST,instance=user)
+        form2 = self.second_form_class(request.POST,instance=estudiante)
+        if form.is_valid() and form2.is_valid():
+            form.save()
+            form2.save()
+        return HttpResponseRedirect(self.get_success_url())
